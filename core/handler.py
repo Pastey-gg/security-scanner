359fc8ff511fbe1bf35a965682fdc7ba3a910010
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-07-26 20:49:13 +1000

    Add message handler

--- core/handler.py
+++ core/handler.py
@@ -13,8 +13,103 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from rabbitmq_amqp_python_client import AMQPMessagingHandler, Event
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from .enums import *
 
 
-class Handler(AMQPMessagingHandler):
-    def on_message(self, event: Event) -> ...: ...
+if TYPE_CHECKING:
+    from pika.adapters import blocking_connection
+    from pika.spec import Basic, BasicProperties
+
+    from pipelines.base import ScanResult
+    from types_.pastes import FilePaste
+
+    from .worker import Worker
+
+
+LOGGER: logging.Logger = logging.getLogger(__name__)
+
+
+class Handler:
+    def __init__(self, worker: Worker) -> None:
+        self.worker = worker
+
+    def __call__(
+        self,
+        channel: blocking_connection.BlockingChannel,
+        method: Basic.Deliver,
+        properties: BasicProperties,
+        body: bytes,
+    ) -> None:
+
+        if not body:
+            LOGGER.warning("Message handler received bad message: No body.")
+            channel.basic_nack(method.delivery_tag)
+            return
+
+        paste_id = body.decode()
+        paste: list[FilePaste] | None = self.fetch_paste(paste_id)
+
+        if not paste:
+            LOGGER.warning("Message handler unable to find paste: %s.", paste_id)
+            channel.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+            return
+
+        # TODO: Error handling for RMQ (discard etc)
+
+        for file in paste:
+            try:
+                result = self.scan(file)
+            except Exception:  # type: ignore
+                # TODO: ...
+                channel.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+                return
+
+            if result:
+                self.process(result)
+                break
+
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+
+    def fetch_paste(self, paste_id: str) -> list[FilePaste] | None:
+        assert self.worker.pool
+        query = """
+        SELECT
+            f.id,
+            f.paste_id,
+            p.created_at,
+            p.deleted_at,
+            p.web,
+            f.name,
+            f."language",
+            f."content",
+            p.deleted_at,
+            f.character_count,
+            f.line_count
+        FROM public.files f
+        JOIN public.pastes p
+        ON p.id = f.paste_id WHERE paste_id = (%s)"""
+
+        with self.worker.pool.connection() as conn, conn.cursor() as cursor:
+            cursor.execute(query, (paste_id,))
+            row: list[FilePaste] | None = cursor.fetchall()
+
+        return row
+
+    def scan(self, paste: FilePaste) -> ScanResult | None:
+        for runner in self.worker.runners:
+            result = runner.scan(paste)
+
+            if not result:
+                continue
+
+            if result.status is not ScanStatus.clear:
+                return result
+
+    def process(self, result: ScanResult) -> ...:
+        # TODO: ...
+        print(result)
