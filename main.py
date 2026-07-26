e32f86be09a20b7e9e2e6c04ad0b01521a54e7c8
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-07-26 20:47:57 +1000

    Add main

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+from core.worker import Worker
+
+
+def main() -> None:
+    with Worker() as worker:
+        worker.run()
+
+
+if __name__ == "__main__":
+    main()
