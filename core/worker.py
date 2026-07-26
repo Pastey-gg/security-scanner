518d59e80d8224f6ad8d8a41307560f5e62c0744
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-07-26 20:48:44 +1000

    Move worker to Pika

--- core/worker.py
+++ core/worker.py
@@ -13,16 +13,120 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from typing import Any, Self
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING, Any, Self
+
+import pika
+from psycopg import Connection
+from psycopg.rows import DictRow, dict_row
+from psycopg_pool import ConnectionPool
+
+from pipelines import _SCANNERS
+
+from .config import CONFIG
+from .handler import Handler
+
+
+if TYPE_CHECKING:
+    from pika.adapters import blocking_connection
+
+    from pipelines import BaseScanner
+
+
+LOGGER = logging.Logger = logging.getLogger(__name__)
 
 
 class Worker:
+    def __init__(self) -> None:
+        self.connection: pika.BlockingConnection | None = None
+        self.channel: blocking_connection.BlockingChannel | None = None
+        self.pool: ConnectionPool[Any] | None = None
+        self._runners: list[BaseScanner] = []
+
+        self._setup: bool = False
+        self._running: bool = False
+
     def __enter__(self) -> Self:
         return self
 
     def __exit__(self, *args: Any, **kwargs: Any) -> None:
         self.close()
 
-    def run(self) -> ...: ...
+    @property
+    def runners(self) -> list[BaseScanner]:
+        return self._runners
 
-    def close(self) -> None: ...
+    def setup(self) -> None:
+        for obj in _SCANNERS:
+            scanner = obj()
+            scanner.compile()
+
+            self._runners.append(scanner)
+
+        self._runners.sort(key=lambda s: s.PRIORITY)
+        self._setup = True
+
+    def run(self) -> None:
+        if self._running:
+            return
+
+        if not self._setup:
+            self.setup()
+
+        config = CONFIG["message_queue"]
+        host = config["host"]
+        port = config["port"]
+
+        dsn = CONFIG["database"]["dsn"]
+        self.pool = ConnectionPool(
+            dsn,
+            check=ConnectionPool.check_connection,
+            open=True,
+            connection_class=Connection[DictRow],
+            kwargs={"row_factory": dict_row},
+        )
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=port))
+        self.channel = self.connection.channel()
+
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(config["queue_name"], Handler(self))
+
+        self._running = True
+
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            LOGGER.warning("Shutting down due to KeyboardInterrupt")
+        except Exception as e:
+            LOGGER.critical("Unhandled exception during worker consuming '%s':\n", e, exc_info=e)
+
+        self.close()
+
+    def close(self) -> None:
+        if not self._running:
+            return
+
+        if not self.connection:
+            return
+
+        if self.channel:
+            try:
+                self.channel.close()  # type: ignore
+            except Exception:
+                pass
+
+        if self.pool:
+            try:
+                self.pool.close()
+            except Exception as e:
+                LOGGER.debug("Ignoring exception while closing Postgres: %s", e)
+
+        try:
+            self.connection.close()
+        except Exception as e:
+            LOGGER.debug("Unhandled exception closing RMQ connection: %s", e, exc_info=e)
+
+        self._running = False
