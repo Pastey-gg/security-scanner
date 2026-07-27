5c7c79a8b207a239fcb49be90e585438c35c046c
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-07-27 16:34:55 +1000

    Update bash script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-curl -L --output-dir "./resources/models" -O "https://huggingface.co/QuantFactory/Llama-Guard-3-1B-GGUF/resolve/main/Llama-Guard-3-1B.Q4_K_M.gguf?download=true"
+FILE="./resources/modelsLlama-Guard-3-1B.Q4_K_M.gguf"
+
+if [ ! -f "$FILE" ]; then
+    curl -L --output-dir "./resources/models" -O "https://huggingface.co/QuantFactory/Llama-Guard-3-1B-GGUF/resolve/main/Llama-Guard-3-1B.Q4_K_M.gguf?download=true"
+fi
