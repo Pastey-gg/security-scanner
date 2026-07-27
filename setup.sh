1ba78ad72e644c4391803a96602b408ba0746cfb
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-07-27 16:35:26 +1000

    Add missing / to filepath

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-FILE="./resources/modelsLlama-Guard-3-1B.Q4_K_M.gguf"
+FILE="./resources/models/Llama-Guard-3-1B.Q4_K_M.gguf"
 
 if [ ! -f "$FILE" ]; then
     curl -L --output-dir "./resources/models" -O "https://huggingface.co/QuantFactory/Llama-Guard-3-1B-GGUF/resolve/main/Llama-Guard-3-1B.Q4_K_M.gguf?download=true"
